7b597c3f6e270960992834c0da14749c003f4cf9
Max Pekarsky <pekarsky.maxim@gmail.com>
2023-07-10 16:32:55 -0400

    server side render HTML with template data

--- server.ts
+++ server.ts
@@ -3,14 +3,87 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const axios = require('axios');                 // make http requests
+const bodyParser = require("body-parser");      // parse request body
+const path = require("path");                   // access paths
 const app: Express = express();
+
+app.use(express.static(path.join(__dirname, "/public/")));
 const port = process.env.PORT;
+
+app.use(function (req, res, next) {
+  // logs request URL
+  var date = new Date();
+  console.log(`==> ${req.method.toUpperCase()} ${req.url} on ${date}`);
+  next();
+});
 
 app.get('/', (req: Request, res: Response) => {
   // res.send('<div>Hello World!<div>');
-  res.sendFile(__dirname + '/public/views/index.html');
+  //res.sendFile(__dirname + '/public/views/index.html');
+  res.send(templateHTMLfile());
+
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+type PokemonResult = {
+  name: string,
+  url: string
+}
+
+type pokemonAPIresponse = {
+  count:  number,
+  next: string | null,
+  previous: string | null,
+  results: PokemonResult[],
+}
+
+
+function templateHTMLfile(){
+      console.log("responding");
+      const url = "https://pokeapi.co/api/v2/pokemon?limit=20";
+      // axios.get(url, {
+      //   headers: {
+      //     Accept: 'application/json',
+      //   },
+      // },)
+      //   .then((res: {data: pokemonAPIresponse}) => res.data)
+      //   .then((data: pokemonAPIresponse)  => {
+      //     console.log(data);
+      //     const pokemonData = data.results.map((pokemon: PokemonResult) => `<p>${pokemon.name}</p>`)
+
+
+
+      //     console.log(html)
+
+      //     return html;
+      //   })
+      //   .catch(err => console.log(err))
+
+      const randomNumbers = new Array(10).fill("a").map((el) => {
+        return `<p>${Math.floor(Math.random() * 1000)}</p>`
+      })
+
+      const randomNumberHTML = randomNumbers.join("");
+
+      const html = `<html>
+        <head>
+          <title>Renderwhere</title>
+          <meta name="description" content="">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="stylesheet" href="views/stylesheet.css">
+          <link rel="icon" href="assets/icon.png" />
+        </head>
+        <body>
+          <div>Hello World!<div>
+          <div>Here are some random numbers: </div>
+          ${randomNumberHTML}
+          <script src="views/script.js"></script>
+        </body>
+      </html>`
+
+      return html;
+}
